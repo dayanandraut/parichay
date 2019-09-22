41bd05926ac350380391e89c887268ae2d484698
dayanandraut <dayanandraut2015@.com>
2019-09-22 18:41:22 +0530

    redesigned intro

--- src/app/introduction/introduction.component.ts
+++ src/app/introduction/introduction.component.ts
@@ -14,5 +14,5 @@ export class IntroductionComponent implements OnInit {
   firstName = "Dayanand";
   lastName = "Raut";
   designantion = "< Full Stack Developer />";
-  intro = "   Young CS graduate having passion of delivering the quality code in agile manner   "
+  intro = "   Graduated in BE-CSE from Sona College of Technology in May 2018, I have been working as a full stack developer at Infosys since June 2018. I keep on learning, unlearning and relearninig the tools and technology to come up with the best solution to the problem.     "
 }
