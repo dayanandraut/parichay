71fd5b3a297389799842772911e9fdb39ea83aed
dayanandraut <dayanandraut2015@.com>
2019-09-22 17:54:19 +0530

    removed useless components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IntroductionComponent } from './introduction/introduction.component';
-import { TechnologyComponent } from './technology/technology.component';
 import { MyProjectComponent } from './my-project/my-project.component';
-import { SkillsComponent } from './skills/skills.component';
 import { MySkillComponent } from './my-skill/my-skill.component';
 import { KnowMeMoreComponent } from './know-me-more/know-me-more.component';
 import { MyFooterComponent } from './my-footer/my-footer.component';
@@ -14,10 +12,8 @@ import { MyFooterComponent } from './my-footer/my-footer.component';
 @NgModule({
   declarations: [
     AppComponent,
-    IntroductionComponent,
-    TechnologyComponent,
-    MyProjectComponent,
-    SkillsComponent,
+    IntroductionComponent,    
+    MyProjectComponent,    
     MySkillComponent,
     KnowMeMoreComponent,
     MyFooterComponent
